302290e8de5268eceb2893f66273dbf79013d70a
Stanislav Surskiy <ssurskiy@gmail.com>
2023-04-29 10:23:45 +0400

    test: adds `test_post_ping_message_dispatches_ping_event`

--- tests/Feature/PingTest.php
+++ tests/Feature/PingTest.php
@@ -17,23 +17,20 @@ class PingTest extends BaseTestCase
             'time' => '2019-08-24T14:15:22Z'
         ];
         $response = $this->postJson(route('ozonseller.webhook'), $payload);
-//        $response->dd();
         $response->assertStatus(200);
     }
 
 
-//    /** @test */
-//    function test_post_ping_message()
-//    {
-//        Event::fake();
-//        $payload = [
-//            'message_type' => 'TYPE_PING',
-//            'time' => '2019-08-24T14:15:22Z'
-//        ];
-//        $this->postJson(route('ozonseller.webhook'), $payload);
-//        Event::assertDispatched(PingEvent::class, function($event) use ($payload) {
-//            return $payload['message_type'] == $event->type;
-//        });
-//
-//    }
+    /** @test */
+    function test_post_ping_message_dispatches_ping_event()
+    {
+        Event::fake();
+        $payload = [
+            'message_type' => 'TYPE_PING',
+            'time' => '2019-08-24T14:15:22Z'
+        ];
+        $this->postJson(route('ozonseller.webhook'), $payload);
+        Event::assertDispatched(PingEvent::class);
+
+    }
 }
