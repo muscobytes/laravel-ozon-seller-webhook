3c1856b9821e68fb24d60508bd29b6f90dacdca7
Stanislav Surskiy <ssurskiy@gmail.com>
2023-05-02 00:56:47 +0400

    test: updates PingTest

--- tests/Feature/PingTest.php
+++ tests/Feature/PingTest.php
@@ -4,33 +4,23 @@ namespace Muscobytes\OzonSeller\Tests\Feature;
 
 use Illuminate\Support\Facades\Event;
 use Muscobytes\OzonSeller\Events\PingEvent;
-use Muscobytes\OzonSeller\Tests\BaseTestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Muscobytes\OzonSeller\Tests\TestCase;
 
-class PingTest extends BaseTestCase
+
+class PingTest extends TestCase
 {
     /** @test */
-    function test_a_basic_request(): void
+    function test_post_ping_message()
     {
-        $payload = [
-            'message_type' => 'TYPE_PING',
-            'time' => '2019-08-24T14:15:22Z'
-        ];
-        $response = $this->postJson(route('ozonseller.webhook'), $payload);
-        $response->assertStatus(200);
-    }
-
-
-    /** @test */
-    function test_post_ping_message_dispatches_ping_event()
-    {
+        $this->withoutExceptionHandling();
         Event::fake();
         $payload = [
             'message_type' => 'TYPE_PING',
             'time' => '2019-08-24T14:15:22Z'
         ];
-        $this->postJson(route('ozonseller.webhook'), $payload);
+        $client_id = '314159265359';
+        $response = $this->postJson(route('ozonseller.webhook', ['client_id' => $client_id]), $payload);
+        $response->assertStatus(200);
         Event::assertDispatched(PingEvent::class);
-
     }
 }
